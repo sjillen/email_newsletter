bf83670d0ca05cecfe5b1cf4003767d2638b956c
Thomas Proust <thomas.proust89@gmail.com>
2024-03-26 10:52:03 +1100

    first test passing

---
+++ tests/health_check.rs
@@ -0,0 +1,26 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute. //
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() {
+    let server = email_newsletter::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
