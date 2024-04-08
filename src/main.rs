a29326bd2e2700d1088c46813b9bfa11034aff4d
Thomas Proust <thomas.proust89@gmail.com>
2024-04-08 11:48:36 +1000

    persist a subscriber into db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use email_newsletter::{configuration::get_configuration, startup::run};
+use sqlx::Pool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection_pool = Pool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
