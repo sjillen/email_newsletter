c71fdc39371d954687fd3a071752b53108436861
Thomas Proust <thomas.proust89@gmail.com>
2024-04-10 16:22:01 +1000

    add tracing and finalize telemetry

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -19,7 +19,7 @@ pub fn get_subscriber<Sink>(
     sink: Sink,
 ) -> impl Subscriber + Send + Sync
 where
-    // This "weird" syntax is a higher-ranked trait bound (HRTB)
+    // higher-ranked trait bound (HRTB)
     // It basically means that Sink implements the `MakeWriter`
     // trait for all choices of the lifetime parameter `'a`
     // Check out https://doc.rust-lang.org/nomicon/hrtb.html
