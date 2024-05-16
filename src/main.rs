2361a84c9ef171ef0d041d774b3f5925333c1750
Thomas Proust <thomas.proust89@gmail.com>
2024-05-16 23:25:08 +1000

    add email_client + big refactor tests

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
 use email_newsletter::{
     configuration::get_configuration,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-    // Panic if we can't read configuration
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
